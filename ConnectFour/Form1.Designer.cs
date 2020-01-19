6cd6aae457faa899c8a93864a5cd77094de08e5c
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-01-19 09:55:46 +0200

    Port to .NET Core (#3)

--- ConnectFour/Form1.Designer.cs
+++ ConnectFour/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,11 +23,12 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.pb56 = new System.Windows.Forms.PictureBox();
             this.pb55 = new System.Windows.Forms.PictureBox();
